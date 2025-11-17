1da2e4ecacb9491958d4d7d7e9034b48e787d521
Aapka Naam <aapka@email.com>
2025-11-17 18:27:18 +0530

    fileupdate

--- Atul/hello.c
+++ Atul/hello.c
@@ -1,7 +1,67 @@
 
-#include <stdio.h>
+// #include <stdio.h>
 
+// int main() {
+    // printf("Hello, World!\n");
+    // return 0;
+// }
+
+//for N number print
+// #include<stdio.h>
+// int main() {
+//     int N;
+
+//     printf("Enter the value of N : ");
+//     scanf("%d", &N);
+
+//     for (int i = 1; i <= N;  i++){
+//         printf("%d", i);
+//     }
+//     return 0;
+// }
+
+// for even number print
+// #include<stdio.h>
+// int main() {
+//     int N;
+
+//     printf("Enter the value of N : ");
+//     scanf("%d",&N);
+
+//     for (int i = 2; i <= N; i += 2){
+//         printf("%d ", i);
+//     }
+//     return 0;
+// }
+
+//print odd number to N
+// #include<stdio.h>
+// int main() {
+//     int N;
+
+//     printf("Enter the value of N : ");
+//     scanf("%d",&N);
+
+//     for (int i = 1; i <= N; i += 2){
+//         printf("%d ", i);
+//     }
+//     return 0;
+// }
+
+// print revers number 
+//  
+
+// sum of first N natural 
+#include<stdio.h>
 int main() {
-    printf("Hello, World!\n");
+    int N, sum = 0;
+
+    printf("Enter the value of N : ");
+    scanf("%d",&N);
+
+    for (int i = 1; i <= N; i++){
+sum += i; }
+printf("Sum of first %d natural numbers is %d ", N, sum);
     return 0;
 }
+
